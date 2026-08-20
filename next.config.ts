8f93f3f8b4176d1515943a5fafa08b95d8a8c749
pw000123 <54762288+pw000123@users.noreply.github.com>
2026-08-20 20:50:55 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/agent-product-page",
-  assetPrefix: "/agent-product-page/",
 };
 
 export default nextConfig;
